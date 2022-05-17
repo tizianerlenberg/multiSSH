e8bfd7857bd5762587c33568a103c218f117498b
Tizian Erlenberg <accounts@erlenberg.net>
2022-05-17 17:59:30 +0200

    test

--- remoteServer.py
+++ remoteServer.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python3
 
+import threading
 import socket
 
+def server():
+    dock_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    dock_socket.bind(('127.0.0.1', 2233))
+    dock_socket.listen(5)
+    client_socket = dock_socket.accept()[0]
+    while True:
+        try:
+            dock_socket.listen(5)
+            local_server_socket = dock_socket.accept()[0]
+
+            threading.Thread(target=forward, args=(client_socket, local_server_socket,)).start();
+            threading.Thread(target=forward, args=(local_server_socket, client_socket,)).start();
+        except:
+            pass
+
+def forward(source, destination):
+    string = ' '
+    while string:
+        string = source.recv(1024)
+        if string:
+            destination.sendall(string)
+        else:
+            source.shutdown(socket.SHUT_RD)
+            destination.shutdown(socket.SHUT_WR)
+
 def main():
-    pass
+    server()
 
 if __name__ == '__main__':
     main()
