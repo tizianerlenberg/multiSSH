19a14d42422065720c488092233df3863bc3479f
Tizian Erlenberg <accounts@erlenberg.net>
2022-05-17 17:59:30 +0200

    test

--- client.py
+++ client.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python3
 
+import threading
 import socket
 
+def server():
+    try:
+        dock_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        dock_socket.bind(('127.0.0.1', 2222))
+        dock_socket.listen(5)
+        while True:
+            client_socket = dock_socket.accept()[0]
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_socket.connect(("192.52.45.151", 2233))
+            threading.Thread(target=forward, args=(client_socket, server_socket,)).start();
+            threading.Thread(target=forward, args=(server_socket, client_socket,)).start();
+    finally:
+        threading.Thread(target=server);
+
+def forward(source, destination):
+    string = ' '
+    while string:
+        string = source.recv(1024)
+        if string:
+            destination.sendall(string)
+        else:
+            source.shutdown(socket.SHUT_RD)
+            destination.shutdown(socket.SHUT_WR)
+
 def main():
-    pass
+    server()
 
 if __name__ == '__main__':
     main()
--- localServer.py
+++ localServer.py
@@ -1,9 +1,37 @@
 #!/usr/bin/env python3
 
+import threading
 import socket
 
+def server():
+    remote_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_server_socket.connect(("192.52.45.151", 2233))
+
+    while True:
+        try:
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_socket.connect(("127.0.0.1", 22))
+
+            threading.Thread(target=forward, args=(remote_server_socket, server_socket,)).start();
+            threading.Thread(target=forward, args=(server_socket, remote_server_socket,)).start();
+            while True:
+                pass
+        except:
+            pass
+
+
+def forward(source, destination):
+    string = ' '
+    while string:
+        string = source.recv(1024)
+        if string:
+            destination.sendall(string)
+        else:
+            source.shutdown(socket.SHUT_RD)
+            destination.shutdown(socket.SHUT_WR)
+
 def main():
-    pass
+    server()
 
 if __name__ == '__main__':
     main()
