3d1c0a4a8f4e32edcca6300a8c59a4d9b62c0df3
Tizian Erlenberg <accounts@erlenberg.net>
2022-05-18 17:26:11 +0200

    ...

--- localServer.py
+++ localServer.py
@@ -3,35 +3,65 @@
 import threading
 import socket
 
+SERVER= ("127.0.0.1", 2233)
+LOCAL_SSH = ("127.0.0.1", 22)
+REMOTE_SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+LOCAL_SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ERROR = ""
+
+def startOfProgram():
+    global REMOTE_SOCK
+    global LOCAL_SOCK
+    global ERROR
+
+    server()
+    while True:
+        if ERROR != "":
+            REMOTE_SOCK.close()
+            LOCAL_SOCK.close()
+            print("Error: ", ERROR)
+            ERROR = ""
+            server()
+
 def server():
-    remote_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    remote_server_socket.connect(("192.52.45.151", 2233))
+    global SERVER
+    global LOCAL_SSH
+    global REMOTE_SOCK
+    global LOCAL_SOCK
+
+    REMOTE_SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    REMOTE_SOCK.connect(SERVER)
 
     while True:
-        try:
-            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server_socket.connect(("127.0.0.1", 22))
+        REMOTE_SOCK.sendall(("offer: " + socket.gethostname()).encode())
+        serverResponse = REMOTE_SOCK.recv(1024).decode()
+        if serverResponse == "request":
+            break
 
-            threading.Thread(target=forward, args=(remote_server_socket, server_socket,)).start();
-            threading.Thread(target=forward, args=(server_socket, remote_server_socket,)).start();
-            while True:
-                pass
-        except:
-            pass
+    LOCAL_SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    LOCAL_SOCK.connect(LOCAL_SSH)
+
+    REMOTE_SOCK.sendall(b"go")
+    threading.Thread(target=forward, args=(REMOTE_SOCK, LOCAL_SOCK,)).start()
+    threading.Thread(target=forward, args=(LOCAL_SOCK, REMOTE_SOCK,)).start()
 
 
 def forward(source, destination):
-    string = ' '
-    while string:
-        string = source.recv(1024)
-        if string:
-            destination.sendall(string)
-        else:
-            source.shutdown(socket.SHUT_RD)
-            destination.shutdown(socket.SHUT_WR)
+    global ERROR
+    try:
+        string = ' '
+        while string:
+            string = source.recv(1024)
+            if string:
+                destination.sendall(string)
+            else:
+                source.shutdown(socket.SHUT_RD)
+                destination.shutdown(socket.SHUT_WR)
+    except Exception as e:
+        ERROR = e
 
 def main():
-    server()
+    startOfProgram()
 
 if __name__ == '__main__':
     main()
