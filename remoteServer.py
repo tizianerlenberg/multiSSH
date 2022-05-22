f9b52f34b88273b6c78be4dde518ada99d1e172e
Tizian Erlenberg <accounts@erlenberg.net>
2022-05-22 22:30:21 +0200

    incremental commit

--- remoteServer.py
+++ remoteServer.py
@@ -4,110 +4,108 @@ import threading
 import socket
 import time
 import queue
-import atexit
+import utils
+import logging
 
-LOCAL = ("0.0.0.0", 2233)
-LOCAL_SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-HOSTS = {}
-CLIENTS = queue.Queue()
-ERROR = "start"
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-def exit_handler():
-    for value in HOSTS.values():
-        value.close()
-    LOCAL_SOCK.close()
+formatter = logging.Formatter('[%(name)s] %(message)s')
 
-def startOfProgram():
-    global LOCAL_SOCK
-    global ERROR
+#file_handler = logging.FileHandler('sample.log')
+#file_handler.setLevel(logging.ERROR)
+#file_handler.setFormatter(formatter)
 
-    atexit.register(exit_handler)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
 
-    while True:
-        if ERROR != "":
-            LOCAL_SOCK.close()
-            ERROR = ""
-            try:
-                server()
-            except Exception as e:
-                ERROR = e
-                print("Error in Thread MAIN: ", e)
-        print("Active Threads: ", threading.active_count())
-        print(HOSTS)
-        time.sleep(1)
+#logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
-def server():
-    global LOCAL
-    global LOCAL_SOCK
-    global HOSTS
+#-------------------------------------------------------------------------------
 
-    LOCAL_SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    LOCAL_SOCK.bind(LOCAL)
-
-    threading.Thread(target=listener).start();
-
-def forward(source, destination):
-    global ERROR
-    myError = "LIGHT ERROR"
-    try:
-        string = ' '
-        while string:
-            string = source.recv(1024)
-            if string:
-                destination.sendall(string)
-            else:
-                source.shutdown(socket.SHUT_RD)
-                destination.shutdown(socket.SHUT_WR)
-    except Exception as e:
-        myError = e
-    finally:
-        print(f"Error in Thread {threading.get_ident()}: {myError}")
-        ERROR = myError
-
-def listener():
-    global LOCAL_SOCK
-    global HOSTS
-    global CLIENTS
-    myError = "LIGHT ERROR"
-    try:
-        while True:
-            LOCAL_SOCK.listen()
-            sock = LOCAL_SOCK.accept()[0]
-            print("hi")
-            msg = sock.recv(1024).decode()
-            print(msg)
-            if msg.startswith("offer"):
-                if (msg[7:] in HOSTS):
-                    HOSTS[msg[7:]].close()
-                HOSTS[msg[7:]] = sock
-            elif msg.startswith("query"):
-                sock.sendall(("\n".join(HOSTS.keys())).encode())
-                threading.Thread(target=handleRequests, args=(sock,)).start()
-    except Exception as e:
-        myError = e
-    finally:
-        print(f"Error in Thread {threading.get_ident()}: {myError}")
-        ERROR = myError
-        sock.close()
-        threading.Thread(target=listener).start();
-
-
-def handleRequests(sock):
-    global HOSTS
+def requestHandler(sock, addr, hosts, clients):
+    logger.info(f"sending host list to client: {addr}")
+    sock.sendall(("\n".join(hosts.keys())).encode())
     request = sock.recv(1024).decode()
     request = request[9:]
-    print(request)
-    hostSock = HOSTS[request]
+    logger.info(f"received request for host: {request}")
+    hostSock = hosts[request]
 
+    logger.info(f"requesting connection from host: {request}")
     hostSock.sendall(b"request")
     response = hostSock.recv(1024).decode()
     if response == "go":
-        print("RECEIVED GO")
+        logger.info(f"received ok from host: {request}")
+        logger.info(f"sending go to client: {addr}")
         sock.sendall(b"go")
-        print("SENT MY GO")
-        threading.Thread(target=forward, args=(hostSock, sock,)).start()
-        threading.Thread(target=forward, args=(sock, hostSock,)).start()
+        logger.info(f"connecting client: {addr} with host: {request}")
+        utils.combinedForward(sock, hostSock)
+        clients[addr]=sock
 
+def listener(sock, waitingConnections):
+    pass
+
+def server(sock):
+    waitingConnections= queue.Queue()
+    connectedClients= {}
+    availableHosts= {}
+    try:
+        logger.info(f"starting listener")
+        listener(sock, waitingConnections)
+        while True:
+            try:
+                conn, msg= waitingConnections.get(block=False)
+            except queue.Empty:
+                pass
+            else:
+                logger.info(f"new {msg} from {conn[1]}")
+                if msg.startswith("offer"):
+                    availableHosts[msg[7:]] = conn
+                elif msg.startswith("query"):
+                    requestHandler(conn[0], conn[1], availableHosts, connectedClients)
+            logger.info(f"nothing to do")
+            time.sleep(1)
+    except:
+        logger.exception(f"CRITICAL ERROR IN SERVER")
+    finally:
+        logger.info(f"cleaning up sockets")
+        logger.info(f"cleaning up connected clients")
+        for key, host in connectedClients.items():
+            logger.info(f"closing client connection: {key}")
+            host.close()
+        logger.info(f"cleaning up queued clients")
+        stop= False
+        while(stop):
+            try:
+                conn= waitingConnections.get(block=False)
+            except queue.Empty:
+                stop= True
+            else:
+                logger.info(f"closing client connection: {conn[1]}")
+                conn[0].close()
+        logger.info(f"cleaning up connected hosts")
+        for key, host in availableHosts.items():
+            logger.info(f"closing host connection: {key}")
+            host.close()
+
+def startOfProgram():
+    addr = ("0.0.0.0", 2233)
+    while True:
+        try:
+            logger.info(f"start")
+            logger.info(f"binding sockets")
+            sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.bind(addr)
+            sock.listen()
+            logger.info(f"starting server")
+            server(sock)
+        except:
+            logger.exception(f"CRITICAL ERROR IN MAIN")
+        finally:
+            logger.info(f"shutdown")
+            sock.close()
+            time.sleep(1)
 
 def main():
     startOfProgram()
