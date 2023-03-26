7c92e9fc22ba27f15f4812394cd05894a8d4f21b
Tizian Erlenberg <mail@erlenberg.net>
2023-03-26 14:28:35 +0200

    not ready yet

--- serverTest/sshServerImpl.py
+++ serverTest/sshServerImpl.py
@@ -1,26 +1,40 @@
 import sshServer
+import utils
+import socket
+import interactive
 
 # own libraries
 import logHandler
 
 logger = logHandler.getSimpleLogger(__name__, streamLogLevel=logHandler.DEBUG)
 
-example_allowed_users = [{'username': 'tunnel',
-                         'password': 'tunnel',
-                         'pkey': None}]
+############## Start ############## 
+
+allowed_users = [{'username': '',
+                  'password': 'tunnel',
+                  'pkey': 'AAAAC3NzaC1lZDI1NTE5AAAAIGjPsR6iO6YxIsSg3Izl76RbTCjDiGZKqn9XRGM8GuVe'}]
+                         
+host=('127.0.0.1', 4446)
 
 def ownServe(chan):
-    while True:
-        answer = chan.recv(1024)
-        if answer == b'':
-            raise
-        logger.info(f"{answer}")
+    chan.sendall('Hello there.\r\n')
 
-serverObj = sshServer.SshServer(('127.0.0.1', 4446), sessionServe=ownServe, allowedUsers=example_allowed_users, allowPkey=False, allowPass=True)
+def ownServe2(chan, chanInfo):
+    src=chan
+    dest = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    dest.connect(chanInfo['destination'])
+    utils.combinedForward(src, dest)
 
-try:
-    serverObj.start()
+def ownServe3(chan):
+    interactive.interactive_shell(chan)
+
+with sshServer.SshServer(
+    host,
+    sessionServe=ownServe,
+    directTcpipServe=ownServe2, 
+    allowedUsers=allowed_users, 
+    allowPass=True, 
+    allowPkey=True
+) as serverObj:
     while True:
         pass
-finally:
-    serverObj.stop()
